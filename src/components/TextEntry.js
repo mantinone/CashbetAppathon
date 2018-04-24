c4fcde7cb8c8d7bfec63ae72a9fa528148ab0128
Max Antinone <mantinone@gmail.com>
2018-04-24 14:47:48 -0700

    Can now submit data to express.

--- src/components/TextEntry.js
+++ src/components/TextEntry.js
@@ -4,14 +4,20 @@ import ReactDOM from "react-dom"
 export default class TextEntry extends React.Component {
   constructor( props ){
     super(props)
+    this.textChange = this.textChange.bind(this)
     this.state = {
       text: ''
     }
   }
 
+  textChange(event){
+    this.setState({text: event.target.value})
+    this.props.getTextData(event.target.value)
+  }
+
   render() {
     return <div>
-      <textarea rows="30" cols="80"></textarea>
+      <textarea rows="30" cols="80" onChange={this.textChange}></textarea>
     </div>
   }
 
--- src/home.js
+++ src/home.js
@@ -1,17 +1,42 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import TextEntry from "./components/TextEntry"
+import Button from "./components/Button"
 
 class HelloMessage extends React.Component {
   constructor( props ){
     super(props)
+    this.getTextData = this.getTextData.bind(this)
+    this.submitData = this.submitData.bind(this)
     this.state = {
-      title: '...Loading'
+      title: '...Loading',
+      text: ''
     }
   }
 
   componentDidMount(){
     this.fetchData()
+  }
+
+  getTextData ( textData ) {
+    this.setState({
+      text: textData
+    })
+  }
+
+  submitData(){
+    const options = {
+      method: "POST",
+      body: JSON.stringify({textData: this.state.text}),
+      mode: 'cors',
+      headers: new Headers({
+        'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+        'Content-Type': 'application/json',
+         }),
+      credentials: 'same-origin'
+    }
+    fetch( 'http://localhost:3000/article', options )
+      .then( response => response.json())
   }
 
   fetchData(){
@@ -39,7 +64,8 @@ class HelloMessage extends React.Component {
     return <div>
       Hello {this.props.name}
       <p> Welcome to {this.state.title} </p>
-      <TextEntry></TextEntry>
+      <TextEntry getTextData={this.getTextData}></TextEntry>
+      <Button name="submit" onClick={this.submitData}></Button>
     </div>
   }
 }
