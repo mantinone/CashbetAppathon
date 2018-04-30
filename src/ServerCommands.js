d1dff14198a6004b749254214164d840060a29a4
Max Antinone <mantinone@gmail.com>
2018-04-29 18:13:49 -0700

    Relative paths are good

--- src/ServerCommands.js
+++ src/ServerCommands.js
@@ -8,12 +8,12 @@ const fetchRequest = ( route = '', method = "GET", body = false ) => {
        }),
     credentials: 'same-origin'
   }
-  
+
   if( body !== false ){
     options = Object.assign( options, { body: JSON.stringify(body) })
   }
 
-  return fetch( `http://localhost:3000/${route}`, options)
+  return fetch( `${window.location.href}${route}`, options)
     .then( response => response.json() )
     .catch( error => console.log(error) )
 }
